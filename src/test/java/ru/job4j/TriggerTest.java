f07893d3fe2ac0e176daedb2b55ee6e2e3470d3f
Xazeq <setezey@gmail.com>
2021-12-06 10:33:39 +0300

    trigger codecov

---
+++ src/test/java/ru/job4j/TriggerTest.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class TriggerTest {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(1, new Trigger().someLogic());
+    }
+}
